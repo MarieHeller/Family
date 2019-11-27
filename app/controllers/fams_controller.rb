07a51432dc1f21209de89af55f855b1a8eefb51c
hamze awawde <ham.awawde@gmail.com>
2019-11-27 12:08:33 +0200

    made search case insensitive

--- app/controllers/fams_controller.rb
+++ app/controllers/fams_controller.rb
@@ -4,8 +4,8 @@ class FamsController < ApplicationController
 
   def index
     if params[:search]
-      @fams = policy_scope(Fam).where(location: params[:search][:location])
-            @markers = @fams.map do |fam|
+      @fams = policy_scope(Fam).where("location ILIKE ?", "%#{params[:search][:location]}%")
+      @markers = @fams.map do |fam|
         {
           lat: fam.latitude,
           lng: fam.longitude,
@@ -26,63 +26,63 @@ class FamsController < ApplicationController
     end
   end
 
-def show
-end
-
-def new
-  @fam = Fam.new
-  authorize @fam
-end
-
-def create
-  @fam = Fam.new(fam_params)
-  authorize @fam
-  @fam.user = current_user
-  if @fam.save
-    create_pictures
-    redirect_to @fam, notice: "Successfully uploaded your family!"
-  else
-    render :new
+  def show
   end
-end
 
-def update
-  @fam.update(fam_params)
-  redirect_to fam_path(@fam)
-end
-
-def edit
-end
-
-def destroy
-  @fam.destroy
-  redirect_to bookings_path
-end
-
-def search
-  @start_date = params[:start_date]
-  @end_date = params[:end_date]
-end
-
-private
-
-def set_fam
-  @fam = Fam.find(params[:id])
-  authorize @fam
-end
-
-def fam_params
-  params.require(:fam).permit(:name, :description, :price, :housing_type, :language, :cultural_experience, :location, :pictures, :start_date, :end_date)
-end
-
-def user_params
-  params.require(:user).permit(:id, :first_name)
-end
-
-def create_pictures
-  photos = params.dig(:fam, :pictures) || []
-  photos.each do |photo|
-    @fam.pictures.create(photo: photo)
+  def new
+    @fam = Fam.new
+    authorize @fam
   end
-end
+
+  def create
+    @fam = Fam.new(fam_params)
+    authorize @fam
+    @fam.user = current_user
+    if @fam.save
+      create_pictures
+      redirect_to @fam, notice: "Successfully uploaded your family!"
+    else
+      render :new
+    end
+  end
+
+  def update
+    @fam.update(fam_params)
+    redirect_to fam_path(@fam)
+  end
+
+  def edit
+  end
+
+  def destroy
+    @fam.destroy
+    redirect_to bookings_path
+  end
+
+  def search
+    @start_date = params[:start_date]
+    @end_date = params[:end_date]
+  end
+
+  private
+
+  def set_fam
+    @fam = Fam.find(params[:id])
+    authorize @fam
+  end
+
+  def fam_params
+    params.require(:fam).permit(:name, :description, :price, :housing_type, :language, :cultural_experience, :location, :pictures, :start_date, :end_date)
+  end
+
+  def user_params
+    params.require(:user).permit(:id, :first_name)
+  end
+
+  def create_pictures
+    photos = params.dig(:fam, :pictures) || []
+    photos.each do |photo|
+      @fam.pictures.create(photo: photo)
+    end
+  end
 end
